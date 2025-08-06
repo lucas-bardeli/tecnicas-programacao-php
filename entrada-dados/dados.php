f309658abd369958fee46390365f69c1280b0d33
Lucas Bardeli <lucasbbsjau@gmail.com>
2025-08-06 12:57:45 -0300

    Entrada de dados pelo html

---
+++ entrada-dados/dados.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_GET) {
+    echo "{$_GET['nome']} <br> {$_GET['idade']}";
+}
+else {
+    // Redireciona para a página index.html
+    header("location:index.html");
+}
+
+// $_POST não vai deixar exibido na url
+
+?>
--- hello.php
+++ hello.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "Hello, World!";
+$nome = "Lucas Bardeli";
+$idade = 19;
+$altura = 1.85;
+$solteiro = true;
+
+echo "<h1 style='color: blue;'>Olá, $nome!\n</h1><br>";
+echo "Idade: $idade\n<br>";
+echo "Altura: $altura\n<br>";
+
+if ($solteiro) {
+    echo "Estado civil: Solteiro";
+}
+else {
+    echo "Estado civil: Compromissado";
+}
 
 ?>
