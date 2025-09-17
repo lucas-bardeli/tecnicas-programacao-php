72f3da95abefb26d0ff4afe38ea898d53002f346
lucas-bardeli <lucasbbsjau@gmail.com>
2025-09-17 09:44:35 -0300

    Movendo o projeto Meu Pet Sumiu para outro repositório.

---
+++ README.md
@@ -0,0 +1,2 @@
+## 🐘 Técnicas programação em PHP:
+Estudos sobre Programação Orientada a Objetos e arquitetura MVC em PHP.
--- meu-pet-sumiu/Controllers/InicioController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class InicioController {
-  public function inicio() {
-    require_once "Views/menu.php";
-  }
-}
-
-?>
